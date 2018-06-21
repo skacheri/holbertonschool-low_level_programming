cc6c5177ecbb23650856e56f2dd6485086348ba6
skacheri <371@holbertonschool.com>
2018-06-21 00:34:49 +0000

    Adding file for task 9

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+* main - entry point
+* Return: value 1
+* write a program to print a string without using puts or printf
+*/
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
