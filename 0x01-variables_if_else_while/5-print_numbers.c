936d2b41de0b379160f8bddfc82166d5c9872ccd
skacheri <371@holbertonschool.com>
2018-06-22 00:06:04 +0000

    Adding file with task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main -starting point
+* program to print all numbers from 0 to 9
+* Return: value 0
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	printf("%i", n);
+	printf("\n");
+	return (0);
+}
