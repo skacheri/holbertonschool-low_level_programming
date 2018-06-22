612ad5c199f2e96fb3c81d249d0ada7bc8166548
skacheri <371@holbertonschool.com>
2018-06-22 00:30:22 +0000

    Adding file for task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main -starting point
+* program to print 0-9 using putchar
+* Return: value 0
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	putchar(n);
+	putchar(10);
+	return (0);
+}
