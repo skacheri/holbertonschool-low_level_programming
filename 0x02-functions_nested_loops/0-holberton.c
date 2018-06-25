ae4a54694e909f55c1ece91dbbbfe4c391747c1b
skacheri <371@holbertonschool.com>
2018-06-25 23:17:57 +0000

    Adding file with task 0

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,22 @@
+#include "Holberton.h"
+
+/**
+* main -Entry point for the program
+* Return: value 0
+* Program to use putchar to print Holberton
+*/
+
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}
