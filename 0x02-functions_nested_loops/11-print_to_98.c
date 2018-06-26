4d9b8f841c4ef902477e0ca312613a92842ed2e8
skacheri <371@holbertonschool.com>
2018-06-26 21:52:07 +0000

    Modifying file contents such that std library is used

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,4 @@
-#include "holberton.h"
-#include <stdlib.h>
+#include <stdio.h>
 
 /**
 * print_to_98 -function to print natural numbers from n to 98
@@ -7,60 +6,20 @@
 */
 void print_to_98(int n)
 {
-	int r, i, j, k;
+	int r;
 
-	if (n > 98)
+	if (n < 98)
 	{
-		for (r = n; r >= 98; r--)
-		{
-
-			i = r / 100;
-			j = (r %  100) / 10;
-			k = r % 10;
-			if (i != 0)
-			_putchar(i + '0');
-			if (j != 0)
-			_putchar(j + '0');
-			_putchar(k + '0');
-			if (r != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
+		for (r = n; r < 98; r++)
+		printf("%i, ", r);
+		if (r == 98)
+		printf("%i\n", r);
 	}
 	else
 	{
-		for (r = n; r <= 98; r++)
-		{
-			if (r >= 0)
-			{
-				i = r / 10;
-				j = r % 10;
-				if (i != 0)
-				_putchar(i + '0');
-				_putchar(j + '0');
-				if (r != 98)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
-			if (r < 0)
-			{
-				i = (r / 10) * -1;
-				j = (r % 10) * -1;
-				_putchar('-');
-				if (i != 0)
-				_putchar(i + '0');
-				_putchar(j + '0');
-				if (r != 98)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
-		}
+		for (r = n; r > 98; r--)
+		printf("%i, ", r);
+		if (r == 98)
+		printf("%i\n", r);
 	}
-	_putchar('\n');
 }
