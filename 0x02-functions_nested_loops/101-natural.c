90c2745b5ad6e7d23ba4701d42c0e7505141e3fc
skacheri <371@holbertonschool.com>
2018-06-27 00:40:05 +0000

    Adding file for task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main -entry point
+* Return: value 0
+*/
+int main(void)
+{
+	int a, n;
+
+	a = 0;
+	for (n = 1; n < 1024; n++)
+	{
+		if ((n / 3) > 0 || (n / 5) > 0)
+		{
+			a += n;
+		}
+	}
+	printf("%i\n", a);
+}
