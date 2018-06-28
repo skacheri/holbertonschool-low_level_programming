44c6d5a6edb54a4d45d8c5efdcdee3b070cd2fe1
skacheri <371@holbertonschool.com>
2018-06-28 22:47:17 +0000

    Adding file with task 9

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,13 +11,15 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
-			printf("Fizz ");
-		else if (i % 5 == 0 && i % 3 != 0)
-			printf("Buzz ");
+		printf("Fizz ");
+		else if (i % 5 == 0 && i % 3 != 0 && i != 100)
+		printf("Buzz ");
 		else if (i % 5 == 0 && i % 3 == 0)
-			printf("FizzBuzz ");
+		printf("FizzBuzz ");
+		else if (i == 100)
+		printf("Buzz");
 		else
-			printf("%i ", i);
+		printf("%i ", i);
 	}
 	printf("\n");
 	return (0);
