79961a31023481d198c5a5d1ad4dbcee211562e8
skacheri <371@holbertonschool.com>
2018-06-28 18:51:11 +0000

    Adding file with task 4

---
+++ 0x03-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+* print_most_numbers -function to print all numbers from 0-9 except 2 & 4
+*/
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i != 50 && i != 52)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
