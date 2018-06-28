d1219bfe33090730aae38ca36a8f9de90a0b3b31
skacheri <371@holbertonschool.com>
2018-06-28 22:16:19 +0000

    Adding file for task 7

---
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+* print_diagonal -function to print diagonal lines n times
+* @n: number as to how many diagonal lines will be printed determined by user
+*/
+void print_diagonal(int n)
+{
+	int row, col;
+	
+	if (n > 0)
+	{
+		for (row = 1; row <= n; row++)
+		{
+			for (col = 1; col < row; col++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
+
