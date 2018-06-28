eb43b2c76cecf4a47e2a9b43a66118e5f5c81ae7
skacheri <371@holbertonschool.com>
2018-06-28 18:30:55 +0000

    Adding file with task 2

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+* mul -function to multiply 2 integers
+* @a: 1st number entered by user to multiply
+* @b: 2nd number entered by user to multiply
+* Return: value of product
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
