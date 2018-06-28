cad03d46947eeab433f94ca433a5adc22254631e
skacheri <371@holbertonschool.com>
2018-06-28 18:41:00 +0000

    Adding file with task 3

---
+++ 0x03-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+* print_numbers -function to print numbers from 0-9
+*/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
